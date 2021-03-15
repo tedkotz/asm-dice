f4c26892d6e7d117d637bea6c0a287e6ae8ebdc9
Theodore Kotz <ted@kotz.us>
2021-03-14 20:12:43 -0400

    Builds cleanly with FASM Generates identical COM file.

--- dice.asm
+++ dice.asm
@@ -1,10 +1,17 @@
+        org     0100h           ;Setting causes fasm to generate a com file
+
+;;;
+;;; Simply Dice Roller program
+;;; Can run from Master Boot Record
+;;; written for FASM (ported from a86)
+;;;
         mov     ah,00h          ;\
         int     1ah             ; seed random number with timer
-        mov     seed w,dx       ;/
+        mov     word [seed], dx ;/
 
 dice:   mov     ax, 0003h       ;clear screen
         int     10h             ;
-        
+
         mov     dx,menu         ;print menu
         mov     ah,09h
         int     21h
@@ -12,64 +19,66 @@ dice:   mov     ax, 0003h       ;clear screen
 lbla:   mov     ah,00h          ;readin a char
         int     16h
 
-        cmp     al,31h          ;check for 1          
+        cmp     al,31h          ;check for 1
         jnz     check2
-        mov     die w,4
+        mov     word [die],4
 check2: cmp     al,32h
         jnz     check3
-        mov     die w,6
+        mov     word [die],6
 check3: cmp     al,33h
         jnz     check4
-        mov     die w,8
+        mov     word [die],8
 check4: cmp     al,34h
         jnz     check5
-        mov     die w,10
+        mov     word [die],10
 check5: cmp     al,35h
         jnz     check6
-        mov     die w,12
+        mov     word [die],12
 check6: cmp     al,36h
         jnz     check7
-        mov     die w,20
+        mov     word [die],20
 check7: cmp     al,37h
         jnz     checkq
-        mov     die w,100
+        mov     word [die],100
 checkq: cmp     al,71h
         jnz     checkt
         int     20h             ;end program
 checkt: cmp     al,74h
         jnz     checkc
-        
+
         mov     dx,dashes       ;print dashes
         mov     ah,09h
         int     21h
-        
-        mov     cx,total w
-        call    WRITE           ;disp total
+
+        mov     cx,word [total]
+        call    writeNum        ;disp total
         jmp     lbla
 checkc: cmp     al,63h
         jnz     cont
-        mov     total w,00h
+        mov     word [total],00h
         jmp     dice
 
 cont:   mov     ax,0fee9h       ;get next random number
-        mul     seed w
-        mov     seed w, ax
-        
-        mul     die w           ;rnd*dice
-        
+        mul     word [seed]
+        mov     word [seed], ax
+
+        mul     word [die]           ;rnd*dice
+
         mov     bx,0ffffh       ;rnd*dice/max_rnd
         div     bx
-        
+
         inc     ax              ;rnd*dice/max_rnd+1
 
-        add     total,ax
+        add     word [total],ax
         mov     cx,ax
-        call    WRITE
+        call    writeNum
         jmp     lbla
 
-seed    dw      1
-die     dw      0ffffh
-total   dw      0
+seed:   dw      1
+die:    dw      0ffffh
+total:  dw      0
 dashes: db      "----",10,13,"$"
 menu:   db      "1-1d4 2-1d6 3-1d8 4-1d10 5-1d12 6-1d20 7-1d100 t-total c-clear q-quit",10,13,"$"
 
+writeNum:
+        ret
